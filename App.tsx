9314e0271790f99d7d65ca91d21874b61c9eb0df
Mateusz Gawlinski <matewka@gmail.com>
2023-06-03 12:03:37 +0200

    Add react-native-paper example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import { PaperProvider, Snackbar } from 'react-native-paper'
+import { ReactNode, useState } from 'react'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaView />
+      <View style={styles.container}>
+        <SnackWrap color="red">
+          <Snack name="Red"/>
+        </SnackWrap>
+        <SnackWrap color="green">
+          <Snack name="Green"/>
+        </SnackWrap>
+        <SnackWrap color="blue">
+          <Snack name="Blue"/>
+        </SnackWrap>
+      </View>
+    </PaperProvider>
   );
+}
+
+function SnackWrap({children, color}: { children: ReactNode, color: string }) {
+  return <View style={[styles.snackWrap, {backgroundColor: color}]}>
+    {children}
+  </View>
+}
+
+function Snack({name}: { name: string }) {
+  const [isOpen, setOpen] = useState(false)
+
+  return <>
+    <Button title={`Open snack "${name}"`} onPress={() => setOpen(true)} color="#fff"/>
+    <Snackbar visible={isOpen} onDismiss={() => setOpen(false)}>
+      Snack bar text
+    </Snackbar>
+  </>
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around'
+  },
+  snackWrap: {
+    marginHorizontal: 32
   },
 });
